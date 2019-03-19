4058748f6f942b8306f64dd163902e7640b88ac0
Luke Harris <lukeharris01170@gmail.com>
2019-03-19 08:57:35 +0000

    Add instagram posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
       options: {
         fonts: ["Source+Code+Pro", "Sarabun:400,400i,500,800"]
       }
+    },
+    {
+      resolve: "gatsby-source-instagram",
+      options: {
+        username: "world.dev"
+      }
     }
   ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
+import { StaticQuery, graphql } from "gatsby";
 import Layout from "../components/layout";
 import PostPreview from "../components/post-preview";
+
 class HomePage extends Component {
   render() {
     return (
       <Layout>
-        <h1>Recent posts</h1>
-        <PostPreview
-          title="Watch this space"
-          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus augue blandit justo placerat, eget varius nibh viverra. Vivamus condimentum erat nisl, vel imperdiet mi tristique vel. Nullam in risus ut augue porttitor tristique. Sed varius massa eget dictum consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis laoreet, lacus sit amet eleifend dictum, enim dolor auctor urna, non hendrerit massa ipsum ut erat. Morbi eget ullamcorper libero, ut lobortis nunc. Curabitur in vulputate quam. Nunc id tempor nisi. Aliquam pharetra egestas elit, at finibus nunc ullamcorper in. Maecenas sed justo ut enim ullamcorper dictum quis non orci. Morbi ut rhoncus nibh. Maecenas sit amet nisl ac tellus hendrerit pharetra quis vel neque. Vestibulum non nibh risus."
-        />
+        <StaticQuery
+          query={graphql`
+            {
+              allInstaNode {
+                edges {
+                  node {
+                    timestamp
+                    caption
+                  }
+                }
+              }
+            }
+          `}
+        >
+          {({ allInstaNode: { edges } }) => (
+            <div
+              css={{
+                maxWidth: 1200,
+                margin: "auto"
+              }}
+            >
+              {edges.map(({ node }) => (
+                <PostPreview
+                  title={new Date(node.timestamp).toString()}
+                  excerpt={node.caption}
+                />
+              ))}
+            </div>
+          )}
+        </StaticQuery>
       </Layout>
     );
   }
