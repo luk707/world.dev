d223ab65fef1d124416129992d9f1185bc45a564
Luke Harris <lukeharris01170@gmail.com>
2019-03-20 08:43:57 +0000

    Fix last row of masonry

--- src/components/masonry.js
+++ src/components/masonry.js
@@ -11,27 +11,29 @@ function Masonry({
   const rowAspectRatioSumsByBreakpoints = itemsPerRowByBreakpoints.map(
     itemsPerRow =>
       // Split images into groups of the given size
-      chunk(aspectRatios, itemsPerRow).map(rowAspectRatios =>
-        // Sum aspect ratios of images in the given row
-        sum(rowAspectRatios)
+      chunk(aspectRatios, itemsPerRow).map(
+        rowAspectRatios =>
+          // Sum aspect ratios of images in the given row
+          sum(rowAspectRatios) + (itemsPerRow - rowAspectRatios.length)
       )
   );
   return items.map((item, i) => {
     const { type, id, aspectRatio, ...rest } = item;
+    const width = rowAspectRatioSumsByBreakpoints.map(
+      // Return a value for each breakpoint
+      (rowAspectRatioSums, j) => {
+        // Find out which row the image is in and get its aspect ratio sum
+        const rowIndex = Math.floor(i / itemsPerRowByBreakpoints[j]);
+        const rowAspectRatioSum = rowAspectRatioSums[rowIndex];
+
+        return `${(aspectRatio / rowAspectRatioSum) * 100}%`;
+      }
+    );
     return (
       <Box
         key={`${type}:${id}`}
         as={itemComponents[type]}
-        width={rowAspectRatioSumsByBreakpoints.map(
-          // Return a value for each breakpoint
-          (rowAspectRatioSums, j) => {
-            // Find out which row the image is in and get its aspect ratio sum
-            const rowIndex = Math.floor(i / itemsPerRowByBreakpoints[j]);
-            const rowAspectRatioSum = rowAspectRatioSums[rowIndex];
-
-            return `${(aspectRatio / rowAspectRatioSum) * 100}%`;
-          }
-        )}
+        width={width}
         css={{ display: "inline-block", fontSize: "1.6rem" }}
         {...rest}
       />
